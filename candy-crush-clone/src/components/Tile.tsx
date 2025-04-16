cf3be510c7b245e443aebeab38afe376d7741360
mjxang <n01615449@humber.ca>
2025-04-15 20:04:27 -0400

    box shadowwwww

--- candy-crush-clone/src/components/Tile.tsx
+++ candy-crush-clone/src/components/Tile.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Tile({candy,candyId}: {candy: string, candyId: number}) {
   return (
-    <div className="h-24 w-24 flex justify-center items-center m-0.5 rounded-lg select-none ">
+    <div className="h-24 w-24 flex justify-center items-center m-0.5 rounded-lg select-none "
+    style={{
+        boxShadow:"inset 5px 5px 15px #062525, inset -5px -5px 15px #aaaab7bb",
+    }}>
         {
             candy && <img src={candy} alt="candy" className="h-20 w-20" candy-id={candyId}/>
         }
