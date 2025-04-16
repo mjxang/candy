0d44646eb0036082e6f5fb1b269d1e885d24b8f7
mjxang <n01615449@humber.ca>
2025-04-15 20:27:55 -0400

    created function for candy popping

--- candy-crush-clone/src/utils/moveCheckLogic.ts
+++ candy-crush-clone/src/utils/moveCheckLogic.ts
@@ -1,14 +1,26 @@
 export const isColumnOfFour = (
     newBoard: string[],
-    boardSize:number,
+    boardSize: number,
     formulaForColumnOfFour: number
 ) => {
-    for (let i: number = 0; i <= formulaForColumnOfFour) i++) {
+    for (let i: number = 0; i <= formulaForColumnOfFour; i++) {
         const columnOfFour: number[] = [
             i,
             i + boardSize,
             i + boardSize * 2,
             i + boardSize * 3,
         ];
+        const decidedColor: string = newBoard[i];
+
+        const isBlank: boolean = newBoard[i] === "";
+
+        if (
+            columnOfFour.every(
+                (candy: number) => newBoard[candy] === decidedColor && !isBlank
+            )
+        ) {
+            columnOfFour.forEach((candy: number) => newBoard[candy] = "");
+            return true;
+        }
     }
 };
