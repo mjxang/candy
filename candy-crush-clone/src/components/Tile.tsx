067396602c55b8523b0e8c60c180b773447b2526
mjxang <n01615449@humber.ca>
2025-04-15 20:01:46 -0400

    styling the candies

--- candy-crush-clone/src/components/Tile.tsx
+++ candy-crush-clone/src/components/Tile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tile({candy,candyId}: {candy: string, candyId: number}) {
   return (
-    <div>
+    <div className="h-24 w-24 flex justify-center items-center m-0.5 rounded-lg select-none ">
         {
             candy && <img src={candy} alt="candy" className="h-20 w-20" candy-id={candyId}/>
         }
